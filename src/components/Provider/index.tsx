776f33a64a647c84bc1c12fccde51ecb2cd3e9ca
Jackson Dolman <mail@dolmios.com>
2025-06-16 22:44:28 -0400

    refactor: remove icon context from provider

--- src/components/Provider/index.tsx
+++ src/components/Provider/index.tsx
@@ -1,10 +1,8 @@
-import { IconContext } from "@phosphor-icons/react";
 import { useEffect, type JSX } from "react";
 
 import packageJson from "../../../package.json";
 import { darkTheme } from "../../stitches.config";
 import { IProvider } from "../../types";
-import { BASE_SIZE } from "../Icon";
 import Portal from "./ProviderPortal";
 import { ProviderStyled, providerReset } from "./styles";
 import { ToastController } from "./ToastController";
@@ -23,17 +21,9 @@ export default function Provider({ children, css, dark }: IProvider): JSX.Elemen
 
   return (
     <ProviderStyled className={isDarkMode ? darkTheme : ""} css={css}>
-      <IconContext.Provider
-        value={{
-          alignmentBaseline: "middle",
-          height: BASE_SIZE,
-          weight: "duotone",
-          width: BASE_SIZE,
-        }}>
-        <ToastController />
-        {children}
-        <Portal />
-      </IconContext.Provider>
+      <ToastController />
+      {children}
+      <Portal />
     </ProviderStyled>
   );
 }
